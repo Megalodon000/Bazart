e3070c7e27d80724ddee1e5ccdd955652a3edbaf
ali mercade <alimercad@gmail.com>
2024-01-31 12:47:24 +0100

    done

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,49 @@
 class ListsController < ApplicationController
+    before_action :authentificate_admin!, except: [:index, :show]
+    before_action :set_list, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @lists = List.all
+    end
+
+    def show
+    end
+
+    def new
+        @list = List.new
+    end
+
+    def create
+        @list = List.new(list_params)
+
+        if @list.save!
+            flash[:success] = "List Created Successfully"
+            redirect_to "/lists/#{list.id}"
+        else
+            flash[:warning] = "List Not Created"
+            redirect_to "/lists/new"
+        end
+    end
+
+    def edit
+        @list = List.find(params[:id])
+    end
+
+    def destroy
+        @list = List.destroy
+        redirect_to "/lists"
+    end
+
+    def update
+    end
+
+    private
+
+    def set_list
+        @list = List.find(params[:id])
+    end
+
+    def list_params
+        params.require(:list).permit(:title, :description)
+    end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,52 @@
 class ReviewsController < ApplicationController
+    before_action :authenticate_user!, except: [:index, :show]
+    before_action :set_review, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @reviews = Review.all
+    end
+
+    def show
+    end
+
+    def new
+        @review = Review.new
+    end
+
+    def create
+        @review = Review.new(review_params)
+
+        if @review.save!
+            flash[:success] = "Review Created Successfully"
+            redirect_to "/reviews/#{review.id}"
+        else
+            flash[:warning] = "Review Not Created"
+            redirect_to "/reviews/new"
+        end
+    end
+
+    def edit
+        if @review.update(review_params)
+            flash[:success] = "Review Updated Successfully"
+            redirect_to "/reviews/#{review.id}"
+        else
+            flash[:warning] = "Review Not Updated"
+            redirect_to "/reviews/#{review.id}/edit"
+        end
+    end
+
+    def destroy
+        @review.destroy
+        redirect_to "/reviews"
+    end
+
+    private
+
+    def set_review
+        @review = Review.find(params[:id])
+    end
+
+    def review_params
+        params.require(:review).permit(:title, :description)
+    end
 end
