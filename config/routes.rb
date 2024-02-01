86b788e6acbb90be4a87fda74df5215fc03552ae
zaid dakir <zaid.dakengineer@gmail.com>
2024-02-01 17:34:05 +0100

    done navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: "pages#home"
 
+  devise_for :users, controllers: { registrations: "registrations" }
+
   resources :products, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   resources :lists, only: [:index, :show, :new, :create, :destroy]
 
