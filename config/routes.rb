15f80128f6cddb36252e12aebb9347f28b12e590
Zakariaboudhaim <zakariaboudhaim@gmail.com>
2024-02-01 10:49:32 +0100

    adding about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  devise_for :users
+
   resources :products, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   resources :lists, only: [:index, :show, :new, :create, :destroy]
 
   get "up" => "rails/health#show", as: :rails_health_check
-
+  get "about" => "pages#about", as: :about
   # Defines the root path route ("/")
   # root "posts#index"
 end
