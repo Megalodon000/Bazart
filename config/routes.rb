53c3a73cf542f1b5646cd694e7f3b7cf622d8e00
zaid dakir <zaid.dakengineer@gmail.com>
2024-02-08 16:14:53 +0100

    add m

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,7 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   resources :users, only: [:index, :show]
+  resources :chatrooms, only: :show do
+    resources :messages, only: :create
+  end
 end
