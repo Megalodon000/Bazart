9f2ae811452a099e09da02e727ca9e0d29f26a15
ali mercade <alimercad@gmail.com>
2024-02-09 12:01:02 +0100

    git commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_06_082327) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_07_110341) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_06_082327) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "chatrooms", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "lists", force: :cascade do |t|
     t.integer "client_id"
     t.bigint "product_id", null: false
@@ -49,6 +55,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_06_082327) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["product_id"], name: "index_lists_on_product_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.bigint "chatroom_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -88,6 +104,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_06_082327) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -96,6 +113,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_06_082327) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "lists", "products"
   add_foreign_key "lists", "users", column: "client_id"
+  add_foreign_key "messages", "chatrooms"
+  add_foreign_key "messages", "users"
   add_foreign_key "products", "users", column: "craftman_id"
   add_foreign_key "reviews", "products"
   add_foreign_key "reviews", "users"
